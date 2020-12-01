3c003c6ea34a8711366407501a6edbd0af64e6be
Barraza44 <diegobarrazapascual@hotmail.com>
2020-11-30 18:07:10 -0600

    added Metals from metals.json file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from "react";
 import Header from "./components/header";
+import Metals from "./metals.json";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      Metals,
       score: 0,
       highScore: 0,
     }
