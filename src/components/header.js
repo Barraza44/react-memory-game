bd3539b6f2902a45f5a8b251d96903677f43e810
Barraza44 <diegobarrazapascual@hotmail.com>
2020-11-30 21:54:25 -0600

    added classes for header component elements

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./header.css"
 
 class Header extends React.Component {
   constructor(props) {
@@ -9,10 +10,10 @@ class Header extends React.Component {
     const { score, highScore } = this.props.score;
     return(
       <header>
-      <h1>Metallic memory game!</h1>
-      <h2>Don't click any metal more than once or you'll loose</h2>
-      <p>Your Score: {score}</p>
-      <p>Your High Score: {highScore}</p>
+      <h1 className="title">Metallic memory game!</h1>
+      <h2 className="second-title">Don't click any metal more than once or you'll loose</h2>
+      <p className="score">Your Score: {score}</p>
+      <p className="high-score">Your High Score: {highScore}</p>
       </header>
     );
   }
