15bd81c1284211b98213fe5cbebb89f24eff5604
Barraza44 <diegobarrazapascual@hotmail.com>
2020-12-04 21:09:44 -0600

    added card component test and move metals to this component's state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import React from "react";
 import Header from "./components/header";
-import Metals from "./metals.json";
+
 import Gameboard from "./components/Gameboard";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      Metals,
       score: 0,
       highScore: 0,
     }
@@ -18,7 +17,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header score={this.state} />
-        <Gameboard metals={this.state.Metals} />
+        <Gameboard  />
       </div>
     );
   }
--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,14 +1,24 @@
 import React from "react";
+import Card from "./card";
+import Metals from "./metals.json";
 
 class Gameboard extends React.Component{
   constructor(props) {
     super(props);
+    this.state = {
+      Metals
+    }
+
   }
 
   render() {
     return (
       <main>
-        <p>Hey there</p>
+        <Card
+          id={this.state.Metals[0].id}
+          image={this.state.Metals[0].image}
+          metal={this.state.Metals[0].metal}
+        />
       </main>
     );
   }
