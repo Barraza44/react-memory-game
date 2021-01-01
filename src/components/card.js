f7b9c9b2faa46ef716769ce81bc67a206d53f5d1
Barraza44 <diegobarrazapascual@hotmail.com>
2020-12-31 18:08:06 -0600

    added card div class and reformatted :D

--- src/components/card.js
+++ src/components/card.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
 
-class Card extends React.Component{
+class Card extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return(
-     <motion.div  id={`card${this.props.id}`} onClick={this.props.handleClick}>
-       <img src={this.props.image} alt=""/>
-       <p>{this.props.metal}</p>
-     </motion.div>
+    return (
+      <motion.div
+        id={`card${this.props.id}`}
+        onClick={this.props.handleClick}
+        class="card-div"
+      >
+        <img src={this.props.image} alt=""/>
+        <p>{this.props.metal}</p>
+      </motion.div>
     );
   }
 }
